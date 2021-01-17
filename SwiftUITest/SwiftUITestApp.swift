2816a67967670e4e3b81df7a4d9d1452586a31c6
maecha <hiiamseiya@gmail.com>
2021-01-17 16:11:20 +0900

    first commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Maechan on 2021/01/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
